0298025ca0b7a7520afe539a036e2384ba7fd27d
johnmichaellim12 <johnmichaellim041202@gmail.com>
2024-11-19 16:55:58 +0800

    javascript event initial

---
+++ script.js
@@ -0,0 +1,8 @@
+const grandparent = document.querySelector(".grandparent")
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
+
+grandparent.addEventListener('click', e => {
+    console.log(e);
+    
+})
