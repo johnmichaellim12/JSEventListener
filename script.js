abb50288d4029db58e2eeb76b44bd7914e3a4970
johnmichaellim12 <johnmichaellim041202@gmail.com>
2024-11-19 17:02:17 +0800

    javascript event initial

--- script.js
+++ script.js
@@ -2,7 +2,27 @@ const grandparent = document.querySelector(".grandparent")
 const parent = document.querySelector(".parent")
 const child = document.querySelector(".child")
 
+// grandparent.addEventListener('click', e => {
+//     console.log(e.target);
+    
+// })
+
 grandparent.addEventListener('click', e => {
-    console.log(e);
+    console.log("Grandparent 1");
+    
+})
+
+parent.addEventListener('click', e => {
+    console.log("Parent 1");
+    
+})
+
+child.addEventListener('click', e => {
+    console.log("Child 1");
+    
+})
+
+document.addEventListener('click', e => {
+    console.log("Document 1");
     
 })
